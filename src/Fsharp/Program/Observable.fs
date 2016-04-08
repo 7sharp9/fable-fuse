8ec62645a050d739fbcf3ff92b0b2b5c406e55c5
Lars Thomas Denstad <ltd@flare.zone>
2016-04-08 23:20:59 +0200

    Committing cause it feels good

--- src/Fsharp/Program/Observable.fs
+++ src/Fsharp/Program/Observable.fs
@@ -14,69 +14,81 @@ module Json =
 
 module Observable =   
 
-    type IObservableType<'T> =        
+    type IObservable<'T> =        
+        abstract member value : 'T with get, set
+        abstract member map<'T,'U> : ('T -> 'U) -> IObservable<'U>
+        abstract member where: ('T -> bool) -> IObservable<'T>
+        [<Emit("$0.map($1...)")>] abstract member mapi<'T, 'U> : ('T * int -> 'U) -> IObservable<'U> // This is NOT implemented correctly
+        abstract member count: unit -> IObservable<int>
+        [<Emit("$0.count($1...)")>] abstract member countWhere: ('T -> bool) -> IObservable<int>        
+        abstract member forEach : ('T -> unit) -> unit        
+        abstract member clear : unit -> unit
+        abstract member indexOf : 'T -> int
+        abstract member contains : 'T -> bool
+        abstract member filter : ('T -> bool) -> IObservable<'T>
+        abstract member expand : unit -> IObservable<'T>
+        abstract member addSubscriber: ('T -> unit) -> unit
+        abstract member removeSubscriber: ('T -> unit) -> unit
+        abstract member length: unit -> int
+        abstract member toArray: unit -> 'T array
+        abstract member getAt : int -> 'T
+        abstract member replaceAt : int -> 'T -> unit
+        abstract member replaceAll : 'T array -> unit
+
+    type IObservable =
         abstract member value : obj with get, set
-        abstract member map<'T,'U> : ('T -> 'U) -> IObservableType<'U>
+        abstract member replaceAll : obj array -> unit
+        abstract member count: unit -> int
+        [<Emit("$0.count($1...)")>] abstract member countWhere: (obj -> bool) -> IObservable
 
     type ObservableFactory =
-        [<Emit("$0($1...)")>] abstract Invoke: name: string -> IObservableType<string>
-        [<Emit("$0($1...)")>] abstract Invoke: ``val``: float -> IObservableType<float>
-        [<Emit("$0($1...)")>] abstract Invoke: num: int -> IObservableType<int>        
-        //[<Emit("$0($1...)")>] abstract Invoke: unit -> IObservableType
+        [<Emit("$0($1...)")>] abstract Invoke: name: string -> IObservable<string>
+        [<Emit("$0($1...)")>] abstract Invoke: ``val``: float -> IObservable<float>
+        [<Emit("$0($1...)")>] abstract Invoke: num: int -> IObservable<int>        
+        [<Emit("$0($1...)")>] abstract Invoke: b: bool -> IObservable<bool>       
+        [<Emit("$0($1...)")>] abstract Invoke<'T> : elements: 'T list -> IObservable<'T list>
+        abstract Invoke : unit -> IObservable
 
-    //let ObservableType:IObservableType = failwith "JS only"
-    
-    type Globals =
+    type private Globals =
         static member observable with get(): ObservableFactory = failwith "JS only" and set(v: ObservableFactory): unit = failwith "JS only"
 
-    //[<Import("", "FuseJS/Observable")>] 
-    //let create =
-    //    Globals.observable.Invoke()
+    
+    [<Import("", "FuseJS/Observable")>] 
+    let createList (elements : obj array) =
+        let emptyList = Globals.observable.Invoke()
+        emptyList.replaceAll(elements)
+        emptyList
+
+    module private implementation =     
+        let createInt (i:int) =
+            Globals.observable.Invoke(i)
+
+        let createString (s:string) = 
+            Globals.observable.Invoke(s)
+
+        let createFloat (n:float) =
+            Globals.observable.Invoke(n)
+
+        let createTypedList<'T> (elements : 'T list) =
+            Globals.observable.Invoke(elements)
+
+        let createObservable () =
+            Globals.observable.Invoke()
+
+
 
     [<Import("", "FuseJS/Observable")>] 
-    let createInt (i:int) =
-        Globals.observable.Invoke(i)
+    let createString string = 
+        implementation.createString string
 
     [<Import("", "FuseJS/Observable")>] 
-    let createString (s:string) = 
-        Globals.observable.Invoke(s)
+    let createnInt int = 
+        implementation.createInt int
 
     [<Import("", "FuseJS/Observable")>] 
-    let createFloat (n:float) =
-        Globals.observable.Invoke(n)
+    let createFloat float = 
+        implementation.createFloat float
 
-#if false
-module FuseJS =
-    type IObservable =  
-        abstract spock: unit -> unit
-        [<Emit("$(0($1...)")>] abstract Invoke: name: string -> IObservable
-        [<Emit("$(0($1...)")>] abstract Invoke: value: float -> IObservable
-        
-    let Observable:IObservable = failwith "JS only"
-
-
-
-module Observable =       
-
-    [<Import("", "FuseJS/Observable")>]
-    //[<Emit("_Observable($0)")>]
-    let create defaultValue : Types.Observable = failwith "JS only"
-
-    //[<Emit("_Observable($0...)")>]    
-    let createMany values : Types.Observable = failwith "JS only"
-
-    [<Emit("$0.replaceAll($1)")>]
-    let replaceAll<'T> (instance : Types.Observable) (value : 'T array) : unit = failwith "JS only"
-
-    [<Emit("$0.replaceAll($1)")>]
-    let replaceAll' (instance : Types.Observable) value : unit = failwith "JS only"
-
-    [<Emit("$0.value")>]
-    let value<'T> (instance : Types.Observable) : 'T = failwith "JS only"
-
-    [<Emit("$1.value = $0")>]
-    let setValue<'T> (value: 'T) (instance : Types.Observable) : unit = failwith "JS only"
-  
-    [<Emit("$1.map($0)")>]
-    let map (f : Types.Observable -> Types.Observable) (instance : Types.Observable) : Types.Observable = failwith "JS only"
-#endif   
+    [<Import("", "FuseJS/Observable")>] 
+    let createObservable () = 
+        implementation.createObservable ()
