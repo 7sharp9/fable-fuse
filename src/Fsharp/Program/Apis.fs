47c7572fb65259a3f35ea7af4a41d68a406ec071
Lars Thomas Denstad <ltd@flare.zone>
2016-04-09 08:50:37 +0200

    Sanity commit

---
+++ src/Fsharp/Program/Apis.fs
@@ -0,0 +1,25 @@
+﻿namespace Fuse
+
+open Fable.Core
+open Fable.Import.JS        
+
+module Console =
+    [<Emit("console.log($0)")>]
+    let log line = failwith "JS only"
+    
+module Json = 
+    [<Emit("JSON.stringify($0)")>]
+    let stringify obj = failwith "JS only"
+
+
+
+module Lifecycle =
+    type [<Import("", "FuseJS/Lifecycle")>] ILifecycle =
+        abstract member onEnteringForeground : unit -> unit with get, set 
+        abstract member onEnteringBackground : unit -> unit with get, set
+
+    let Lifecycle : ILifecycle = failwith "JS only"
+
+    let onEnteringForeground (handler : unit -> unit) = 
+        Lifecycle.onEnteringBackground = handler
+    
--- src/Fsharp/Program/Observable.fs
+++ src/Fsharp/Program/Observable.fs
@@ -2,15 +2,6 @@
 
 open Fable.Core
 open Fable.Import.JS    
-    
-module Console =
-    [<Emit("console.log($0)")>]
-    let log line = failwith "JS only"
-    
-module Json = 
-    [<Emit("JSON.stringify($0)")>]
-    let stringify obj = failwith "JS only"
-
 
 module Observable =   
 
@@ -43,48 +34,28 @@ module Observable =
 
     type ObservableFactory =
         abstract Invoke<'T> : element: 'T -> IObservable<'T>
+        abstract Invoke<'T> : unit -> IObservable<'T>
         abstract Invoke : unit -> IObservable
+        [<Emit("(0, _Observable2.default)(...$1)")>] // This doesn't work
+        abstract InvokeList<'T> : elements : 'T array -> IObservable<'T>
 
     type private Globals =
         static member observable with get(): ObservableFactory = failwith "JS only" and set(v: ObservableFactory): unit = failwith "JS only"
 
+    [<Import("", "FuseJS/Observable")>] 
+    let createWith<'T> (elem : 'T) = 
+        Globals.observable.Invoke(elem)
+ 
+    [<Import("", "FuseJS/Observable")>]
+    let createTyped<'T> =
+        Globals.observable.Invoke<'T>()
+
+    [<Import("", "FuseJS/Observable")>] 
+    let create () = 
+        Globals.observable.Invoke()
+
+    // This doesn't work
+    [<Import("", "FuseJS/Observable")>] 
+    let createList<'T> (elements : 'T array) =
+        Globals.observable.InvokeList<'T>(elements)
     
-    [<Import("", "FuseJS/Observable")>] 
-    let createList (elements : obj array) =
-        let emptyList = Globals.observable.Invoke()
-        emptyList.replaceAll(elements)
-        emptyList
-
-    module private implementation =     
-        let createInt (i:int) =
-            Globals.observable.Invoke(i)
-
-        let createString (s:string) = 
-            Globals.observable.Invoke(s)
-
-        let createFloat (n:float) =
-            Globals.observable.Invoke(n)
-
-        let createTypedList<'T> (elements : 'T list) =
-            Globals.observable.Invoke(elements)
-
-        let createObservable () =
-            Globals.observable.Invoke()
-
-
-
-    [<Import("", "FuseJS/Observable")>] 
-    let createString string = 
-        implementation.createString string
-
-    [<Import("", "FuseJS/Observable")>] 
-    let createnInt int = 
-        implementation.createInt int
-
-    [<Import("", "FuseJS/Observable")>] 
-    let createFloat float = 
-        implementation.createFloat float
-
-    [<Import("", "FuseJS/Observable")>] 
-    let createObservable () = 
-        implementation.createObservable ()
