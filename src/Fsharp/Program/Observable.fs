7dfc5d49c5c555ffadcc5892c9feb7066f62937a
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-04-18 14:19:32 +0200

    Fix Observable definitions

--- src/Fsharp/Program/Observable.fs
+++ src/Fsharp/Program/Observable.fs
@@ -1,5 +1,6 @@
 ﻿namespace Fuse
 
+open System
 open Fable.Core
 open Fable.Import.JS    
 
@@ -9,15 +10,17 @@ module Observable =
         abstract member value : 'T with get, set        
         abstract member map<'T,'U> : ('T -> 'U) -> IObservable<'U>
         abstract member where: ('T -> bool) -> IObservable<'T>
-        [<Emit("$0.map($1...)")>] abstract member mapi<'T, 'U> : ('T * int -> 'U) -> IObservable<'U> // This is NOT implemented correctly
+        // Lambdas with more than one argument must be converted to delegates to be called
+        // properly from JS: See https://github.com/fsprojects/Fable/blob/master/docs/source/docs/interacting.md#calling-f#-code-from-javascript
+        [<Emit("$0.map($1)")>] abstract member mapi<'T, 'U> : Func<'T,int,'U> -> IObservable<'U>
         abstract member count: unit -> IObservable<int>
-        [<Emit("$0.count($1...)")>] abstract member countWhere: ('T -> bool) -> IObservable<int>     
+        [<Emit("$0.count($1)")>] abstract member countWhere: ('T -> bool) -> IObservable<int>     
         abstract member add: 'T -> unit
         abstract member remove: 'T -> unit
         abstract member tryRemove: 'T -> bool
         abstract member removeAt: int -> unit
         abstract member removeWhere: ('T -> bool) -> unit
-        abstract member refreshAll: IObservable<'T> -> ('T -> 'T -> bool) -> ('T -> 'T -> unit) -> ('T -> 'T) -> unit
+        abstract member refreshAll: IObservable<'T> -> Func<'T,'T,bool> -> Func<'T,'T,unit> -> ('T -> 'T) -> unit
         abstract member forEach : ('T -> unit) -> unit        
         abstract member clear : unit -> unit
         abstract member indexOf : 'T -> int
@@ -34,45 +37,28 @@ module Observable =
 
     type IUnsafeObservable<'T> = 
         inherit IObservable<'T>
-        [<Emit("$0.value = $1")>] abstract member valueOverride : obj with get, set
+        [<Emit("$0.value{{=$1}}")>] abstract member valueOverride : obj with get, set
 
     type IObservable = 
         inherit IObservable<obj>                
 
     type private ObservableFactory =
-        abstract Invoke<'T> : element: 'T -> IObservable<'T>
-        abstract Invoke<'T> : unit -> IObservable<'T>
-        abstract InvokeUnsafe<'T> : element: 'T -> IUnsafeObservable<'T>
-        abstract InvokeUnsafe<'T> : unit -> IUnsafeObservable<'T>
-        abstract Invoke : unit -> IObservable
-        [<Emit("(0, _Observable2.default)(...$1)")>] // This doesn't work
-        abstract InvokeList<'T> : elements : 'T array -> IObservable<'T>
+        [<Emit("$0($1)")>] abstract Invoke<'T> : element: 'T -> IObservable<'T>
+        [<Emit("$0()")>] abstract Invoke<'T> : unit -> IObservable<'T>
+        [<Emit("$0($1)")>] abstract InvokeUnsafe<'T> : element: 'T -> IUnsafeObservable<'T>
+        [<Emit("$0()")>] abstract InvokeUnsafe<'T> : unit -> IUnsafeObservable<'T>
+        [<Emit("$0()")>] abstract Invoke : unit -> IObservable
+        [<Emit("$0($1...)")>] abstract InvokeList<'T> : [<ParamArray>] elements : 'T array -> IObservable<'T>
 
-    type private Globals =
-        static member observable with get(): ObservableFactory = failwith "JS only" and set(v: ObservableFactory): unit = failwith "JS only"
+    // An empty string is also equivalent to "default"
+    [<Import("default", "FuseJS/Observable")>]
+    let private observable: ObservableFactory = failwith "JS only"
 
-    [<Import("", "FuseJS/Observable")>] 
-    let createWith<'T> (elem : 'T) = 
-        Globals.observable.Invoke(elem)
- 
-    [<Import("", "FuseJS/Observable")>]
-    let createTyped<'T> =
-        Globals.observable.Invoke<'T>()
-
-    [<Import("", "FuseJS/Observable")>] 
-    let createUnsafeWith<'T> (elem : 'T) = 
-        Globals.observable.InvokeUnsafe(elem)
- 
-    [<Import("", "FuseJS/Observable")>]
-    let createUnsafeTyped<'T> =
-        Globals.observable.InvokeUnsafe<'T>()
-
-    [<Import("", "FuseJS/Observable")>] 
-    let create () = 
-        Globals.observable.Invoke()
-
-    // This doesn't work
-    [<Import("", "FuseJS/Observable")>] 
-    let createList<'T> (elements : 'T array) =
-        Globals.observable.InvokeList<'T>(elements)
+    let createWith<'T> (elem : 'T) = observable.Invoke(elem)
+    let createTyped<'T> = observable.Invoke<'T>()
+    let createUnsafeWith<'T> (elem : 'T) = observable.InvokeUnsafe(elem)
+    let createUnsafeTyped<'T> = observable.InvokeUnsafe<'T>()
+    let create () = observable.Invoke()
+    // A problem has been found in Fable (to be fixed) if this function is not inlined
+    let inline createList<'T> (elements : 'T array) = observable.InvokeList<'T>(elements)
     
